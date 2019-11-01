499feaf3e0d891ae666ccf9a020bec861668da58
vars007 <shanbhagvarun55@gmail.com>
2019-11-01 18:45:26 -0500

    https://leetcode.com/problems/reorder-data-in-log-files/

---
+++ Practice/ReorderDatainLogFiles.java
@@ -0,0 +1,57 @@
+/*You have an array of logs.  Each log is a space delimited string of words.
+
+For each log, the first word in each log is an alphanumeric identifier.  Then, either:
+
+Each word after the identifier will consist only of lowercase letters, or;
+Each word after the identifier will consist only of digits.
+We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that each log has at least one word after its identifier.
+
+Reorder the logs so that all of the letter-logs come before any digit-log.  The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.  The digit-logs should be put in their original order.
+
+Return the final order of the logs.
+
+ 
+
+Example 1:
+
+Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+ 
+
+Constraints:
+
+0 <= logs.length <= 100
+3 <= logs[i].length <= 100
+logs[i] is guaranteed to have an identifier, and a word after the identifier.*/
+
+class ReorderDatainLogFiles {
+    public String[] reorderLogFiles(String[] logs) {
+        
+         Comparator<String> myComp = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                int s1valIndex = s1.indexOf(' ');
+                int s2valIndex = s2.indexOf(' ');
+                char s1fc = s1.charAt(s1valIndex+1);
+                char s2fc = s2.charAt(s2valIndex+1);
+                
+                if (s1fc <= '9') {
+                    if (s2fc <= '9') return 0;
+                    else return 1;
+                }
+                if (s2fc <= '9') return -1;
+                
+                int preCompute = 
+                    s1.substring(s1valIndex+1).compareTo(s2.substring(s2valIndex+1));
+                
+                if (preCompute == 0) return
+                    s1.substring(0,s1valIndex).compareTo(s2.substring(0,s2valIndex));
+                return preCompute;
+            }
+        };
+        
+        Arrays.sort(logs, myComp);
+        return logs;
+        
+    }
+}
